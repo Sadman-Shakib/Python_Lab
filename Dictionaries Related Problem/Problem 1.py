0fbef040822a8203d6cbf3db98323ce2eb6f49d8
Sadman Sakib <sadmansakib345@gmail.com>
2024-02-25 17:17:49 +0600

    added a new problem at dictionary problem problem 1

---
+++ Dictionaries Related Problem/Problem 1.py
@@ -0,0 +1,21 @@
+"""
+    you are given a result of a section
+    now your task is count number of student whos grade point is A+
+
+    Sample Input                                    sample ouutput
+
+    list = ["A+", "A", "A-", "A+", "B-", "A+"]      
+"""
+
+#solution
+
+ls = ["A+", "A", "A-", "A+", "B-", "A+"]
+cnt = dict()
+ans = 0
+for i in ls:
+    if i in cnt:
+        cnt[i] += 1
+    else :
+        cnt[i] = 1
+
+print(cnt["A+"])
