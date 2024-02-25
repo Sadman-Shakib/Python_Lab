759055fbdde15aaf15a067db7a759156a400b58a
Sadman Sakib <sadmansakib345@gmail.com>
2024-02-25 18:11:01 +0600

    added problem 4 in dict related problem

---
+++ Dictionaries Related Problem/Problem 4.py
@@ -0,0 +1,26 @@
+"""
+    you are given two list list1 and list2
+    now your task is print the element which are not present in list 2 at least 2 times
+
+    sample input                          sample output
+
+    ls1 = [1, 2, 4, 5, 4, 8, 9]           1 2
+    ls2 = [1, 1, 2, 2, 3, 4, 5, 6]
+
+"""
+
+
+ls1 = [1, 2, 4, 5, 4, 8, 9]
+ls2 = [1, 1, 2, 2, 3, 4, 5, 6]
+
+cnt = dict()
+for i in ls2:
+    if i in cnt:
+        cnt[i] += 1
+    else:
+        cnt[i] = 1
+
+for i in ls1:
+    if i not in cnt: continue
+    if cnt[i] > 1:
+        print(i, end = " ")
