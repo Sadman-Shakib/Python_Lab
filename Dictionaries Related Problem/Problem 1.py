d29db69bae2ff723b3b4acd25591d8970e16b1c3
Sadman Sakib <sadmansakib345@gmail.com>
2024-02-25 16:24:06 +0600

    added problem 4 at set related problem and aded folder dictionaries related problem

---
+++ Dictionaries Related Problem/Problem 1.py
@@ -0,0 +1,9 @@
+"""
+    you are given an list. now your task is find the most frequent element of the list
+    if multiple element have same frequency print any of them
+
+    sample input                      sample output
+
+    2 3 4 5 2 4 2 2 1
+"""
+
---
+++ Set Related Problem/Problem 4.py
@@ -0,0 +1,22 @@
+"""
+    you are given some unique integers . now your task is implement a function that 
+    takes a sequences and an integer as parameter and return sum of first n smallest
+    element 
+
+    sample input                        sample output
+
+    sequence = 2, 1, 3, 4, 0, 5         3     
+    n = 3
+     
+"""
+
+#solution
+
+def fun(arr, n):
+    s = set(arr)
+    l = list(s)
+    return sum(l[:n])
+
+a = [2, 1, 3, 4, 0, 5]
+
+print(fun(a, 3))
