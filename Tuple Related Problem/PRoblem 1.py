bacf4b706baaf9e1e71d2f3a9ef663eb72b9a973
Sadman Sakib <sadmansakib345@gmail.com>
2024-02-26 20:10:20 +0600

    added new tuple releated problem

---
+++ Tuple Related Problem/PRoblem 1.py
@@ -0,0 +1,22 @@
+"""
+
+    you are given two tuple define a function where fucntion takes two tuple as 
+    parameter and return middle value of tuple if we concatanate those two tuples
+
+    Sample Input                                 Sample Output
+    
+    5, 2, 4, 2, 4                                7
+    7, 3, 5, 1. 9
+"""
+
+#solution
+
+def getMid(x, y):
+    temp = tuple()
+    temp = x + y
+    return temp[len(temp) // 2]
+
+t1 = (5, 2, 4, 2, 4)
+t2 = (7, 3, 5, 1, 9)
+
+print(getMid(t1, t2))
