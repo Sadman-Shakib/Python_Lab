0c8260cff87df1de34fcd974c1b0adecfbee2c7d
Sadman Sakib <sadmansakib345@gmail.com>
2024-02-26 20:13:40 +0600

    added 2nd tuple releated problem

---
+++ Tuple Related Problem/Problem 2.py
@@ -0,0 +1,18 @@
+"""
+    you are given two tuple concatanate those two tuples and print the sum of values
+
+    Sample Input                              Sample Output
+
+    5, 2, 4, 2, 4                                7
+    7, 3, 5, 1. 9
+
+"""
+
+#solution
+
+t1 = (5, 2, 4, 2, 4)
+t2 = (7, 3, 5, 1, 9)
+
+ans = tuple(t1 + t2)
+
+print(sum(ans))
